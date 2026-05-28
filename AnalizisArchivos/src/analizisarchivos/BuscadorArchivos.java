dfac14aa5d05519946b9711a9c74e9963f27ee5c
Andrés Díaz <andresalbertodiazrevelo@gmail.com>
2026-05-28 15:59:44 -0600

    Mas clases, no gui

--- AnalizisArchivos/src/analizisarchivos/BuscadorArchivos.java
+++ AnalizisArchivos/src/analizisarchivos/BuscadorArchivos.java
@@ -3,11 +3,35 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package analizisarchivos;
-
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 /**
  *
  * @author user
  */
 public class BuscadorArchivos {
+    public List<String> buscar (File directorioRaiz, String textoBusqueda){
+    List <String> resultados = new ArrayList<>();
+    buscar(directorioRaiz, textoBusqueda.toLowerCase(), resultados);
+    return resultados;
+    }
+    
+    private void buscar (File directorio, String textoBusqueda, List<String> resultados){
+        File[] contenido = directorio.listFiles();
+        if (contenido == null) return;
+        for (File elemento: contenido){
+            if (elemento.isDirectory()){
+                buscar(elemento,textoBusqueda,resultados);
+            }
+            else if (elemento.isFile()){
+                if (elemento.getName().toLowerCase().contains(textoBusqueda)){
+                    resultados.add(elemento.getAbsolutePath());
+                }
+            }
+        }
+    }
+    
+    
     
 }
--- AnalizisArchivos/src/analizisarchivos/Contador.java
+++ AnalizisArchivos/src/analizisarchivos/Contador.java
@@ -3,11 +3,64 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package analizisarchivos;
-
+import java.io.File;
 /**
  *
  * @author user
  */
 public class Contador {
     
-}
+    
+    public ResultadoConteo analizar(File directorioRaiz){
+        ResultadoConteo resultado = new ResultadoConteo();
+        recorrerDirectorio(directorioRaiz,resultado);
+        return resultado;        
+    }
+    
+    
+    private void recorrerDirectorio (File directorio, ResultadoConteo resultado){
+        File[] contenido = directorio.listFiles();
+        if (contenido == null) 
+            return;
+        
+        for (File elemento : contenido){
+            if (elemento.isDirectory()){
+                recorrerDirectorio (elemento,resultado);
+            }
+            else if (elemento.isFile()){
+                clasificarArchivo(elemento, resultado);
+            }
+        }
+        
+    }
+        private void clasificarArchivo (File archivo, ResultadoConteo resultado){
+            String ext = obtenerExtension (archivo.getName());
+            
+            switch (ext.toLowerCase()){
+                case "txt":
+                    resultado.incrementarTxt();
+                    break;
+                case "java":
+                    resultado.incrementarJava();
+                    break;
+                case "pdf":
+                    resultado.incrementarPdf();
+                    break;
+                default:
+                    resultado.incrementarOtros();
+                    break;
+            }
+        }
+        
+        
+        private String obtenerExtension (String nombreArchivo){
+            int pos = nombreArchivo.lastIndexOf('.');
+            if (pos < 0 || pos == nombreArchivo.length() - 1)
+                return "";
+            return nombreArchivo.substring(pos + 1 );
+        }
+        
+        
+    }
+    
+
